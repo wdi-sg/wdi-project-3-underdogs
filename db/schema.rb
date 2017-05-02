8639e8b828a66e5c3830873dcb1d89accd2ff861
bao ling koh <cocoinquire@gmail.com>
2017-05-02 18:56:24 +0800

    completed sprint 2 fixed migrations and added tables to db on postico

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,41 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-
 ActiveRecord::Schema.define(version: 20170502101238) do
-
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "bank_account_infos", force: :cascade do |t|
+    t.string   "email"
+    t.string   "bank_name"
+    t.string   "bank_currency"
+    t.integer  "account_no"
+    t.string   "name_on_account"
+    t.string   "branch_code"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+  end
+
+  create_table "incomes", force: :cascade do |t|
+    t.string   "email"
+    t.integer  "monthly_income"
+    t.integer  "monthly_savings_goal"
+    t.integer  "final_savings_goal"
+    t.datetime "created_at",           null: false
+    t.datetime "updated_at",           null: false
+  end
+
+  create_table "rewards", force: :cascade do |t|
+    t.string   "merchant"
+    t.string   "merchant_code"
+    t.string   "item"
+    t.integer  "value"
+    t.string   "expiry_date"
+    t.string   "category"
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
+  end
 
   create_table "transactions", force: :cascade do |t|
     t.string   "email"
@@ -26,7 +53,31 @@ ActiveRecord::Schema.define(version: 20170502101238) do
     t.integer  "transaction_no"
     t.datetime "created_at",        null: false
     t.datetime "updated_at",        null: false
+  end
 
+  create_table "users", force: :cascade do |t|
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.datetime "created_at",                          null: false
+    t.datetime "updated_at",                          null: false
+    t.string   "first_name"
+    t.string   "last_name"
+    t.date     "dob"
+    t.string   "gender"
+    t.string   "nationality"
+    t.string   "address"
+    t.string   "postal_code"
+    t.integer  "mobile_no"
+    t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
 end
