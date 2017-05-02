28efc08afd05e49d9133675977d48ec7176291a9
bao ling koh <cocoinquire@gmail.com>
2017-05-02 18:58:25 +0800

    tried db seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,24 +6,24 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-# 1.times do
-#   Reward.create(
-#   merchant: Faker::Company.name,
-#   merchant_code: Faker::Company.suffix,
-#   item: Faker::Commerce.promotion_code,
-#   value: rand(100),
-#   expiry_date:Faker::Date.forward(180),
-#   category:Faker::Commerce.department
-#   )
-# end
-#
-# 5.times do
-#   BankAccountInfo.create(
-#   email: Faker::Internet.email,
-#   bank_name: Faker::Bank.name,
-#   bank_currency: Faker::Address.state_abbr,
-#   account_no: rand(1000),
-#   name_on_account: Faker::StarWars.character,
-#   branch_code: Faker::Bank.swift_bic
-#   )
-# end
+1.times do
+  Reward.create(
+  merchant: Faker::Company.name,
+  merchant_code: Faker::Company.suffix,
+  item: Faker::Commerce.promotion_code,
+  value: rand(100),
+  expiry_date:Faker::Date.forward(180),
+  category:Faker::Commerce.department
+  )
+end
+
+5.times do
+  BankAccountInfo.create(
+  email: Faker::Internet.email,
+  bank_name: Faker::Bank.name,
+  bank_currency: Faker::Address.state_abbr,
+  account_no: rand(1000),
+  name_on_account: Faker::StarWars.character,
+  branch_code: Faker::Bank.swift_bic
+  )
+end
