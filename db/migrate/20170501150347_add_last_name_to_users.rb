d264def9b17ac67aea93c6732f4649f35ed06d27
Cara Chew <carachew@gmail.com>
2017-05-03 14:59:49 +0800

    deleted migration file schema items and seeds and dropped database

--- db/migrate/20170501150347_add_last_name_to_users.rb
+++
@@ -1,7 +0,0 @@
-class AddLastNameToUsers < ActiveRecord::Migration[5.0]
-  def change
-    add_column :users, :last_name, :string
-    add_column :users, :add_dob_to_users, :string
-    add_column :users, :dob, :date
-  end
-end
--- db/migrate/20170501150809_add_mobile_no_to_users.rb
+++
@@ -1,5 +0,0 @@
-class AddMobileNoToUsers < ActiveRecord::Migration[5.0]
-  def change
-    add_column :users, :mobile_no, :integer
-  end
-end
