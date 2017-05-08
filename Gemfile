89c4b44495c409265a4ef880da9ca46e5237a9ce
bao ling koh <cocoinquire@gmail.com>
2017-05-08 12:23:54 +0800

    want to pull something

--- Gemfile
+++ Gemfile
@@ -77,4 +77,4 @@ gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 
 # ruby '2.2.2'
 
-ruby '2.2.3'
+ruby '2.2.2'
--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -6,7 +6,7 @@ class TransactionsController < ApplicationController
     @sumofmoney = Transaction.select("transacted_amount", "transacted_date").where(user_id: current_user.id)
     @total =  Transaction.select("transacted_amount").where(user_id: current_user).sum("transacted_amount")
     @credit = (@total*0.0075).round(0)
-    @date = Transaction.select("created_at").where("created_at >?", 2/4/2017 )
+    # @date = Transaction.select("created_at").where("created_at >?", 2/4/2017 )
   end
 
   def new
