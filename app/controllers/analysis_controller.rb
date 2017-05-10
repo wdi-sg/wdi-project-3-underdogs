609ea7f7438806a7e938222184ab29377138e4d9
Cara Chew <carachew@gmail.com>
2017-05-11 00:37:30 +0800

    need to still fix the time period bug

--- app/controllers/analysis_controller.rb
+++ app/controllers/analysis_controller.rb
@@ -7,12 +7,29 @@ before_action :authenticate_user!
     @income = Income.where(user_id: current_user.id)[0]
     @final = Income.select("final_savings_goal").where(user_id: current_user.id).sum("final_savings_goal")
     @monthly = Income.select("monthly_savings_goal").where(user_id: current_user.id).sum("monthly_savings_goal")
-    @monthlyinterest = (@monthly*0.003*12)+@monthly
-    @originaltime = @final/@monthlyinterest
+    @monthlyinterest = (@monthly*0.003*12).to_f + @monthly
+    @originaltime = @final.to_f/@monthlyinterest.to_f
     @saved = Transaction.select("transacted_amount").where(user_id: current_user.id).sum("transacted_amount")
     @interest = @saved*0.003*12
     @totalsaved = @saved + @interest
-    @finaltime = @final/@totalsaved
+    @finaltime = @final.to_f/@totalsaved
+
+    puts '@final is the final savings goal of the user'
+    puts @final
+    puts '@saved is the total savings amount in Cache account'
+    puts @saved
+    puts '@interest 3% of the 1% for Cash Back x 12 months'
+    puts @interest
+    puts '@totalsaved is the total savings plus cash back'
+    puts @totalsaved
+    puts '@monthly is the Monthly Savings Goal entered by user'
+    puts @monthly
+    puts '@monthlyinterest is the Monthly savings goal plus cash back if the user topped up monthly'
+    puts @monthlyinterest
+    puts '@originaltime is the final savings goal divided by the monthly interest plus cash back if user topped up mthly'
+    puts @originaltime
+    puts '@finaltime is the final savings goal divided by mthly savings goal entered by user'
+    puts @finaltime
   end
 
   def update
