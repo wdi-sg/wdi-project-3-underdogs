7923d43010b3c1fce5119a0caa380ad534cdfaaf
bao ling koh <cocoinquire@gmail.com>
2017-05-10 20:37:59 +0800

    siao liao

--- app/controllers/analysis_controller.rb
+++ app/controllers/analysis_controller.rb
@@ -5,6 +5,14 @@ before_action :authenticate_user!
   def index
     @user = User.find(current_user)
     @income = Income.where(user_id: current_user.id)
+    @final = Income.select("final_savings_goal").where(user_id: current_user.id).sum("final_savings_goal")
+    @monthly = Income.select("monthly_savings_goal").where(user_id: current_user.id).sum("monthly_savings_goal")
+    @months = @final/@monthly rescue 0
+    @deposit = Transaction.select("transacted_amount").where(user_id:current_user.id).sum("transacted_amount")
+    @interest = @deposit*0.0025.round(0)
+    @totalsave = @deposit+@interest
+    @month = @final/@totalsave rescue 0
+
   end
 
   def update
--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -5,10 +5,9 @@ class TransactionsController < ApplicationController
     @user = User.find(current_user)
     @sumofmoney = Transaction.select("transacted_amount", "transacted_date").where(user_id: current_user.id)
     @total =  Transaction.select("transacted_amount").where(user_id: current_user).sum("transacted_amount")
-    @credit = (@total*0.007).round(0)
-    # @date = Transaction.select("created_at").where("created_at >?", 2/4/2017 )
+    @credit = (@total*0.0075*12).round(0)
     @rewarding = @user.rewards.select("value").sum("value")
-    @cashback = (@total*0.003).round(0)
+    @cashback = (@total*0.0025*12).round(0)
   end
 
 
