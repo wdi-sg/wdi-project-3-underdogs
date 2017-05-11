1ab0c5ef47a7b168cd68c994a941bc01ae651b4c
iancwe <crubbishhute@gmail.com>
2017-05-11 12:51:06 +0800

    done done

--- app/assets/javascripts/pages.js
+++ app/assets/javascripts/pages.js
@@ -1,6 +1,9 @@
 $(document).on('turbolinks:load', function () {
+  //parallax for homepage
   $('.parallax').parallax()
+  // mobile responsive sideNav
   $('.button-collapse').sideNav()
+  // back to to btn for homepage
   $('#tpBtn').click(function(){
     $("body, html").animate({
     scrollTop: $("body").position().top
